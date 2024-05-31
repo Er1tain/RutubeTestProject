0ee85e539bb7280dc8a3f89fdaf0dd3f93695bd0
unknown <sanyapridava@mail.ru>
2024-05-31 18:28:37 +0300

    Structuring component

--- front/src/website_pages/Evaluation.tsx
+++ front/src/website_pages/Evaluation.tsx
@@ -1,12 +1,22 @@
 import Header from "../components/Header";
 import Divider from "../components/Divider";
 import React from "react";
-import '../styles/Evaluation.css'
+import '../styles/Evaluation.css';
+import '../styles/choice_button.css';
 
 export default class Evaluation extends React.Component {
 
+    Buttons() {
+        let variants = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        return <div className={"List_button"}>
+            {variants.map(variant => {
+                return <button className={"Choice_button"}>{variant}</button>
+            })}
+        </div>
+    }
 
-    Content(){
+    Content({children}: any) {
+
         return <div id={"Evaluation_Content"}>
             <div id={"Evaluation_Content_Frame"}>
                 <div id={"Evaluation_Content_Frame_img"}></div>
@@ -15,18 +25,25 @@ export default class Evaluation extends React.Component {
                 <div className={"center-text-content"}>
                     <h1>Уважаемый клиент!</h1>
                     <p>
-                        Запрос закрыт. Пожалуйста, оцените качество предоставленного сервиса по данному обращению, используя шкалу от 0 до 9, где 0 является «Хуже некуда» и 9 — «Отлично».
+                        Запрос закрыт. Пожалуйста, оцените качество предоставленного сервиса по данному обращению,
+                        используя шкалу от 0 до 9, где 0 является «Хуже некуда» и 9 — «Отлично».
                     </p>
+                    <div className={"Selection"}>
+                        {children}
+                        <div className={"Labels"}></div>
+                    </div>
                 </div>
             </div>
         </div>
     }
 
     render() {
-        return<>
+        return <div>
             <Header/>
             <Divider/>
-            <this.Content/>
-        </>
+            <this.Content>
+                <this.Buttons/>
+            </this.Content>
+        </div>
     }
 }

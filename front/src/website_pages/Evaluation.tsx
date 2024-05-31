e80230ad934a3c815b45f06eef13b3c38e0a7e9c
unknown <sanyapridava@mail.ru>
2024-05-31 16:52:03 +0300

    Add new content in main page

--- front/src/website_pages/Evaluation.tsx
+++ front/src/website_pages/Evaluation.tsx
@@ -1,7 +1,32 @@
 import Header from "../components/Header";
+import Divider from "../components/Divider";
+import React from "react";
+import '../styles/Evaluation.css'
 
-export default function  Evaluation() {
-    return <>
-        <Header/>
-    </>
+export default class Evaluation extends React.Component {
+
+
+    Content(){
+        return <div id={"Evaluation_Content"}>
+            <div id={"Evaluation_Content_Frame"}>
+                <div id={"Evaluation_Content_Frame_img"}></div>
+            </div>
+            <div id={"Question"}>
+                <div className={"center-text-content"}>
+                    <h1>Уважаемый клиент!</h1>
+                    <p>
+                        Запрос закрыт. Пожалуйста, оцените качество предоставленного сервиса по данному обращению, используя шкалу от 0 до 9, где 0 является «Хуже некуда» и 9 — «Отлично».
+                    </p>
+                </div>
+            </div>
+        </div>
+    }
+
+    render() {
+        return<>
+            <Header/>
+            <Divider/>
+            <this.Content/>
+        </>
+    }
 }

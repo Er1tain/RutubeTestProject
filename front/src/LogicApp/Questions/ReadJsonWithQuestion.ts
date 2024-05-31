cdcbd04bd4215bba43c32bb445e0dba3e4bfa3f5
unknown <sanyapridava@mail.ru>
2024-05-31 22:19:46 +0300

    Create json with questions and answers

---
+++ front/src/LogicApp/Questions/ReadJsonWithQuestion.ts
@@ -0,0 +1,6 @@
+import json_data from "../../components/Questions/questions_answers.json";
+
+export default function ReadJson() {
+
+
+}
--- front/src/components/Questions/Test.tsx
+++ front/src/components/Questions/Test.tsx
@@ -1,4 +1,12 @@
+import {useEffect} from "react";
+import ReadJson from "../../LogicApp/Questions/ReadJsonWithQuestion";
+
 export default function Test() {
+
+    useEffect(() => {
+        ReadJson();
+    }, []);
+
     return (
         <div id={"Test"}>
             <h1 id={"Label_test"}>Пожалуйста, ответьте на дополнительные вопросы.</h1>

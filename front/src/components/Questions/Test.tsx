14af980ea111d097e8c0937bec6679802076d856
unknown <sanyapridava@mail.ru>
2024-06-01 20:01:55 +0300

    change styles

--- front/src/components/Questions/Test.tsx
+++ front/src/components/Questions/Test.tsx
@@ -1,16 +1,16 @@
-import {useEffect} from "react";
+import {createElement, useEffect, useState} from "react";
 import ReadJson from "../../LogicApp/Questions/ReadJsonWithQuestion";
 
 export default function Test() {
+    const [questions_answers, setQuestAns] = useState(createElement('div'));
 
-    useEffect(() => {
-        ReadJson();
-    }, []);
+    //Parse JSON and set state of questions and answers
+    useEffect(() => ReadJson(setQuestAns),[]);
 
     return (
         <div id={"Test"}>
             <h1 id={"Label_test"}>Пожалуйста, ответьте на дополнительные вопросы.</h1>
-
+            {questions_answers}
         </div>
     )
 }

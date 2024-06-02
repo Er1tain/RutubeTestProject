c8e0ba8a5861707ee047e8e18cfb823bd06aedd8
unknown <sanyapridava@mail.ru>
2024-06-03 01:11:26 +0300

    create page what see when test finishing in past

--- front/src/website_pages/Dejavu.tsx
+++ front/src/website_pages/Dejavu.tsx
@@ -1,4 +1,42 @@
+import Header from "../components/Evaluation/Header";
+import Divider from "../components/Evaluation/Divider";
+import styled from "styled-components";
+import Image from "../components/Dejavu/image";
+
+const Center = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+
+    width: 1053px;
+    max-width: 1053px;
+    height: 433px;`
+
+const FrameImage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 12px 0px;
+
+    width: 1053px;
+    height: 256px;
+
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;`
 
 export default function Dejavu() {
-    return <></>
+    return (
+        <div>
+            <Header/>
+            <Divider/>
+            <Center>
+                <FrameImage>
+                    <Image/>
+                </FrameImage>
+            </Center>
+        </div>
+    );
 }

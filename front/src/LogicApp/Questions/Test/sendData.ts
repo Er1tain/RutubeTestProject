157a0c80a118bb95c2bf84b6011f5fe679dab0b2
unknown <sanyapridava@mail.ru>
2024-06-03 00:49:46 +0300

    output data from localstorage

--- front/src/LogicApp/Questions/Test/sendData.ts
+++ front/src/LogicApp/Questions/Test/sendData.ts
@@ -5,5 +5,16 @@ export default function sendData(navigate: NavigateFunction) {
         let key = localStorage.key(i);
         if (key != null && localStorage.getItem(key) === null) return
     }
+
+    let data_from_localStorage = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if (key == "evaluation") data_from_localStorage.push({evaluation: localStorage.getItem("evaluation")});
+        else data_from_localStorage.push({
+            question: localStorage.key(i),
+            response: localStorage.getItem(key != null ?key: "undefined")
+        })
+    }
+    console.log(data_from_localStorage);
     navigate("/feedback")
 }

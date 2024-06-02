30f548ccebf994c4fa0624bd685bbb73c6334219
unknown <sanyapridava@mail.ru>
2024-06-02 22:31:48 +0300

    select button having self color different another

--- front/src/LogicApp/Questions/ReadJsonWithQuestion.tsx
+++ front/src/LogicApp/Questions/ReadJsonWithQuestion.tsx
@@ -7,8 +7,10 @@ import {
     TextButtonAnswers
 } from "../../styles/Questions/Test";
 import getArrayData from "./GetJSONData";
+import setColorActivButton from "./Test/setColorActivButton";
 
-export default function Readjson(setQuestAns:  React.Dispatch<React.SetStateAction<any>>) {
+export default function ReadJson(setQuestAns:  React.Dispatch<React.SetStateAction<any>>) {
+
     let json_data = getArrayData();
     let questions: React.JSX.Element[] = [];
     /* questions = [<div>1. XXXXXXXX<div>, <div>2. XXXXXXXXXX</div>]*/
@@ -21,10 +23,12 @@ export default function Readjson(setQuestAns:  React.Dispatch<React.SetStateActi
 
         let Buttons: React.ReactElement[] = [];
         json_data[i]["answers"].forEach((answers)=>{
+
             //value: (text in button:number of question
-            Buttons.push(i != 0 ? <ButtonAnswers value={`${answers}:${i+1}`}>{answers}</ButtonAnswers> : <TextButtonAnswers value={`${answers}:${i+1}`}>{answers}</TextButtonAnswers>);
+            Buttons.push(i != 0 ? <ButtonAnswers value={`${answers}:${i+1}`}>{answers}</ButtonAnswers> :
+                <TextButtonAnswers value={`${answers}:${i+1}`}>{answers}</TextButtonAnswers>);
         })
-        let answer = i != 0 ? <Answers>{Buttons}</Answers> : <TextAnswers>{Buttons}</TextAnswers>
+        let answer = i != 0 ? <Answers onClick={event=>setColorActivButton(event)}>{Buttons}</Answers> : <TextAnswers onClick={event=>setColorActivButton(event)}>{Buttons}</TextAnswers>
         answers.push(answer);
     }
 

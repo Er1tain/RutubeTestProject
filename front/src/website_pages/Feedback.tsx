0a9720687ac3dfd661f9bf812c9a2a30d1fe18ab
unknown <sanyapridava@mail.ru>
2024-06-02 23:12:15 +0300

    create block with image

--- front/src/website_pages/Feedback.tsx
+++ front/src/website_pages/Feedback.tsx
@@ -1,12 +1,29 @@
 import Header from "../components/Evaluation/Header";
 import Divider from "../components/Evaluation/Divider";
+import styled from "styled-components";
+
+const FrameImage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 12px 0px;
+
+    width: 1053px;
+    height: 256px;
+
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;`
 
 export default function Feedback() {
     return (
         <div>
             <Header/>
             <Divider/>
+            <FrameImage>
 
+            </FrameImage>
         </div>
     );
 }
